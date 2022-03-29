cceacbbe14e6392da5126d4eb6a5f320ea1bedf2
javi cisneros <javing77@gmail.com>
2022-03-28 20:32:47 -0500

    Fin de la sesion 2

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -8,9 +9,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DinamicosComponent implements OnInit {
 
-  constructor() { }
+
+  miFormulario: FormGroup = this.fb.group(
+    {
+      nombre    : [ , [Validators.required, Validators.minLength(3) ] ] ,
+      favoritos : this.fb.array( [
+        ['Metal Gear', Validators.required],
+        ['Prueba', Validators.required]
+      ], Validators.required)
+    }
+
+  );
+
+  nuevoFavorito: FormControl = this.fb.control('', Validators.required);
+
+  get favoritosArr(){
+    return this.miFormulario.get('favoritos') as FormArray;
+  }
+
+  constructor( private fb: FormBuilder ) { }
 
   ngOnInit(): void {
+  }
+
+  campoNoValido(campo: string){
+    return this.miFormulario.controls[campo].errors &&
+          this.miFormulario.controls[campo].touched;
+  }
+
+  borrar(i: number){
+      this.favoritosArr.removeAt(i)
+  }
+
+  agregarFavorito(){
+    if( this.nuevoFavorito.invalid ) {return;}
+
+    // Estas lineas hacen lo mismo
+    // this.favoritosArr.push(  new FormControl(this.nuevoFavorito.value, Validators.required) );
+    this.favoritosArr.push(  this.fb.control(this.nuevoFavorito.value, Validators.required) );
+
+    this.nuevoFavorito.reset();
+
+  }
+
+  guardar(){
+    if(this.miFormulario.invalid){
+      this.miFormulario.markAllAsTouched();
+    }
+
+    console.log(this.miFormulario.controls['nombre'].value ) ;
+
+
   }
 
 }
--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,4 +1,6 @@
+import { ThisReceiver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
@@ -8,9 +10,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwitchesComponent implements OnInit {
 
-  constructor() { }
+  miFormulario: FormGroup = this.fb.group({
+      genero        : [ , Validators.required ],
+      notificaciones: [ , Validators.required ],
+      condiciones   : [ , [Validators.required, Validators.requiredTrue] ]
+  });
+
+  persona = {
+    genero: 'M',
+    notificaciones: false,
+  }
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    // no se usa setValue porque persona no tiene la propiedad personas y esto genera error
+    this.miFormulario.reset( this.persona );
+    // Otra forma de hacer que una propiedad tenga valor
+    // this.miFormulario.reset( {
+    //       ...this.persona,
+    //       condiciones: false});
+
+
+    // subscribirnos para que la persona cambien cuando se cambia en miFormulario
+    this.miFormulario.valueChanges.subscribe( ({  condiciones, ...elResto}) => {
+      //delete form.condiciones
+      this.persona = elResto;
+    })
+
+    //Subscrinos a una propiedad en especifico
+    // this.miFormulario.get('condiciones')?.valueChanges.subscribe( newV => { })
+
+  }
+
+  guardar(){
+    const formValue = { ...this.miFormulario.value }
+    delete formValue.notificaciones;
+    console.log(formValue);
+
+    this.persona = formValue;
+
   }
 
 }
